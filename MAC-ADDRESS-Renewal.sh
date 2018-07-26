5737a775f0f374b7b0ad11453a1a18b272dc4618
zukkie777 <41683291+zukkie777@users.noreply.github.com>
2018-07-27 01:37:08 +0900

    Add files via upload

---
+++ MAC-ADDRESS-Renewal.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#WORKDIR=/home/suzuki/tasuzuki/work
+WORKDIR=/Users/zukkie/work
+BACKUPDIR=$WORKDIR/backup
+
+#NMAP_DIR=/usr/share/nmap/
+NMAP_DIR=/usr/local/bin/nmap
+NMAP_MAC_ADDR_DIR=/usr/local/share/nmap
+NMAP_MAC_PREFIXES=nmap-mac-prefixes
+CURRENT_NMAC_MAC=$NMAP_MAC_ADDR_DIR/$NMAP_MAC_PREFIXES
+
+#PERL_PATH=/bin/perl
+#CURL_PATH=/bin/curl
+PERL_PATH=/usr/bin/perl
+CURL_PATH=/usr/bin/curl
+
+echo `date "+%Y%m%d%H%M%S"` > DATE_TIME
+
+cd $WORKDIR
+
+
+echo "------------------------"
+echo "MAC ADDRESS  GET START!!"
+echo "------------------------"
+
+$CURL_PATH -OL  http://standards.ieee.org/regauth/oui/oui.txt
+
+echo "------------------------"
+echo "Backup the Current MAC ADDRESS DATA."
+echo "------------------------"
+
+sudo cp $CURRENT_NMAC_MAC $BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME`
+
+echo "------------------------"
+echo "Convert format OUI and update NMAP-MAC-Prefixes. "
+echo "------------------------"
+
+sudo  $PERL_PATH make-mac-prefixes.pl oui.txt $CURRENT_NMAC_MAC
+
+echo "------------------------"
+echo "Check the difference for  before and after"
+echo "------------------------"
+
+diff $CURRENT_NMAC_MAC $BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME` >$BACKUPDIR/`cat DATE_TIME`_diff
+ 
