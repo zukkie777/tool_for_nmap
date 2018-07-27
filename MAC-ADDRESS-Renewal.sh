cdcd85563722dd12b47e5195a6d4e04f4d8e0fcb
zukkie777 <41683291+zukkie777@users.noreply.github.com>
2018-07-27 12:12:29 +0900

    Add files via upload
    update issue
     copy oui.txt

--- MAC-ADDRESS-Renewal.sh
+++ MAC-ADDRESS-Renewal.sh
@@ -10,7 +10,7 @@ NMAP_DIR=/usr/local/bin/nmap
 NMAP_MAC_ADDR_DIR=/usr/local/share/nmap
 NMAP_MAC_PREFIXES=nmap-mac-prefixes
 CURRENT_NMAC_MAC=$NMAP_MAC_ADDR_DIR/$NMAP_MAC_PREFIXES
-BACKUP_NMAC_MAC=$BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME`
+#BACKUP_NMAC_MAC=$BACKUPDIR/nmap-mac-prefixes_`cat DATE_TIME`
 
 #PERL_PATH=/bin/perl
 #CURL_PATH=/bin/curl
@@ -23,17 +23,19 @@ cd $WORKDIR
 
 
 echo "------------------------"
-echo "MAC ADDRESS  GET START!!"
+echo "NEW-MAC-ADDRESS-LIST GET START!!"
 echo "------------------------"
 
-$CURL_PATH -OL  http://standards.ieee.org/regauth/oui/oui.txt
+#$CURL_PATH -OL  http://standards.ieee.org/regauth/oui/oui.txt
+cp oui.txt today_dir/oui.txt_`cat DATE_TIME`
 
 echo "------------------------"
-echo "Backup the Current MAC ADDRESS DATA."
+echo "Backup the Current-MAC-ADDRESS-DATA."
 echo "------------------------"
 
 #sudo cp $CURRENT_NMAC_MAC $BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME`
-cat auth.txt |sudo -S cp $CURRENT_NMAC_MAC $BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME`
+#cat auth.txt |sudo -S cp $CURRENT_NMAC_MAC $BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME`
+cat auth.txt |sudo -S cp $CURRENT_NMAC_MAC $BACKUPDIR/nmap_mac_prefixes_`cat DATE_TIME`
 
 echo "------------------------"
 echo "Convert format OUI and update NMAP-MAC-Prefixes. "
@@ -46,6 +48,5 @@ echo "------------------------"
 echo "Check the difference for  before and after"
 echo "------------------------"
 
-#diff $CURRENT_NMAC_MAC $BACKUPDIR/$NMAP_MAC_PREFIXES_`cat DATE_TIME` >$BACKUPDIR/`cat DATE_TIME`_diff
-diff $CURRENT_NMAC_MAC  $BACKUP_NMAC_MAC > $BACKUPDIR/`cat DATE_TIME`_diff
+diff $CURRENT_NMAC_MAC $BACKUPDIR/nmap_mac_prefixes_`cat DATE_TIME` >$BACKUPDIR/`cat DATE_TIME`_diff
  
