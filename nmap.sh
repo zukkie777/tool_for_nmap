af375cd643e456759f5c6b41e33658b1fade9c01
zukkie777 <zukkie77@hotmail.com>
2018-09-06 20:46:36 +0900

    fix

--- nmap.sh
+++ nmap.sh
@@ -1,39 +1,91 @@
 #!/bin/bash
+#
+export PATH="$PATH/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin"
+export LANG=C
 
-# Remove file
-if test -e  error-file ; then
-   rm  error-file
-   echo "remove error-file"
-fi
-if test -e  NWADDR ; then
-   rm  NWADDR
-   echo "remove NWADDR"
+# define directory & files
+WORKDIR=/usr/local/etc/nmap-tool
+TMPDIR=$WORKDIR/tmp-dir
+NMAPLOGFILE=$TMPDIR/nmap-logfile
+NWADDR=$TMPDIR/nwaddr
+IPADDRTMP=$TMPDIR/ipaddr-tmp
+
+### resorce information ###
+### cpu,memory etc,etc
+./sar.sh &
+
+sleep 360
+
+# Remove TMP file
+if [ test -e  $TMPDIR/* ; then
+   cd $TMPDIR
+   rm *
+   echo "tmp-file"
 fi
 
-WORKDIR=/home/tasuzuki/work
 
 cd $WORKDIR
 
 # ---------------------------
 # for debug line
-#NWADDR="192.168.0.53"
-#NWADDR=""
-#NWADDR= `ip r |grep "dev eno1 proto"|grep -v "default via"|cut -f 1 -d " "`
+#$NWADDR="192.168.0.53"
+#$NWADDR=""
+#$NWADDR= `ip r |grep "dev eno1 proto"|grep -v "default via"|cut -f 1 -d " "`
 # ---------------------------
 #
 # Search network Address
-#echo "192.168.0.53" > NWADDR
-/usr/sbin/ip r |grep "dev eno1 proto"|grep -v "default via"|cut -f 1 -d " " > NWADDR
+   echo "-----------------------------------"                                                                          >  $NMAPLOGFILE
+   echo "search network address : `date "+%Y/%m/%d/%H:%M:%S"`"                                                         >> $NMAPLOGFILE
+#echo "192.168.0.53" > $NWADDR
+#
+# for single Interface(Please modify divice name)
+#/usr/sbin/ip r |grep "dev eno1 proto"|grep -v "default via"|cut -f 1 -d " " 				 > $NWADDR
+#
+# for MacOS
+#ipcalc -n `ip addr   | grep "inet " |awk '{print $2}'|grep -v ^127.0.0.1`|grep Network |awk '{print $2}' > $NWADDR
 
-if test !-s NWADDR ; then
-#if test -z "$NWADDR" ; then
-   echo "no ip error!  " > error-file
+# for Multi Interface
+#ip addr   | grep "inet " |awk '{print $2}'|grep -v ^127.0.0.1 > 							$IPADDRTMP
+ip addr   | grep "inet " |awk '{print $2}'|grep -v ^127.0.0.1 > 							$IPADDRTMP
+   echo "search network finish : `date "+%Y/%m/%d/%H:%M:%S"`"                                                              >> $NMAPLOGFILE
+   echo "-----------------------------------"                                                                              >> $NMAPLOGFILE
+   echo "NW-address calclation : `date "+%Y/%m/%d/%H:%M:%S"`"                                                              >> $NMAPLOGFILE
+while read line;
+ do  ipcalc -np   $line;
+ done < $IPADDRTMP|sed -e "N;s/\n/,/g"|awk -F',' '{print $2,$1}'|sed -s "s/NETWORK=//"|sed -s "s/ PREFIX=/\//"> 	$NWADDR
+   echo "NW-address calclation finish : `date "+%Y/%m/%d/%H:%M:%S"`"                                                       >> $NMAPLOGFILE
+   echo "-----------------------------------"                                                                              >> $NMAPLOGFILE
+
+
+
+
+if test !-s $NWADDR ; then
+   echo "no ip error!  " > $TMPDIR/error-file
 
 # Scan network
 else
-   echo "nmap start" 
+   echo "-----------------------------------"                                                                            >> $NMAPLOGFILE
+   #echo "nmap-start : `date "+%Y/%m/%d/%H:%M:%S"`"                                                                      >> $NMAPLOGFILE
+   echo "nmap-start : `date "+%Y/%m/%d/%H:%M"`"                                                                          >> $NMAPLOGFILE
+
    #cat auth.txt | sudo -S nmap -sV -A $NWADDR -oX /tmp/result_scan_"%Y%m%d-%H%M%S".xml
    #cat auth.txt | sudo -S nmap -T4 -A  $NWADDR -oX /tmp/`date "+%Y%m%d%H%M%S"`.xml
-   cat auth.txt | sudo -S /usr/bin/nmap -T4 -A -v  `cat NWADDR` -oX /tmp/`date "+%Y%m%d%H%M%S"`.xml
+   #cat auth.txt | sudo -S /usr/bin/nmap -T4 -A -v  `cat $NWADDR` -oX /tmp/`date "+%Y%m%d%H%M%S"`.xml
+   #cat auth.txt | sudo -S nmap -T4 -A -v  `cat $NWADDR` -oX /tmp/`date "+%Y%m%d%H%M%S"`.xml
+
+# Multi Interface discovery
+   cat auth.txt | sudo -S nmap -T4 -A -v -iL $NWADDR -oX /tmp/`date "+%Y%m%d%H%M%S"`.xml;
+
+   #echo "nmap-finish : `date "+%Y/%m/%d/%H:%M:%S"`"                                                                      >> $NMAPLOGFILE
+   echo "nmap-finish : `date "+%Y/%m/%d/%H:%M"`"                                                                          >> $NMAPLOGFILE
+
+###
+
+sleep 480
+
+###
+### Mapping result-of-sar_and_nmap-log
+###
+  ./sar-result_and_nmap-log.sh
 
 fi
